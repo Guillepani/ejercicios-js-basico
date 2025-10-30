4a79bc4f1c8dbae9f03a5f6ff88ceb941f929133
Guille <guillempaniagua@gmail.com>
2025-10-30 20:18:01 +0000

    Ejercicio 9 completo

---
+++ katas-js/ejercicio9.js
@@ -0,0 +1,15 @@
+/*Implementa la función denominada sumAll que toma un array de números como
+argumento y devuelve la suma de todos los números de la matriz.*/
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(numbersArray) {
+  let total = 0;
+  
+  for (const number of numbersArray) {
+    total += number;
+  }
+  
+  return total;
+}
+
+console.log(sumAll(numbers));
