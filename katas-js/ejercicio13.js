4d40c2fe11bd25af8add2e177f0b9944a8fab6b7
Guille <guillempaniagua@gmail.com>
2025-10-31 16:29:14 +0000

    Ejercicio 13 completo

---
+++ katas-js/ejercicio13.js
@@ -0,0 +1,33 @@
+/*Crea una función que reciba por parámetro un array y el valor que desea comprobar 
+que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true
+y la posición de dicho elemento y por la contra un false.*/
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+
+function findElement(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      return {
+        exists: true,
+        position: i
+      };
+    }
+  }
+  return {
+    exists: false,
+    position: -1
+  };
+}
+
+console.log(findElement(nameFinder, 'Tony'));
