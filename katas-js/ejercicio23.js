ad61f90041193cd0d2e064546925eea618eae60e
Guille <guillempaniagua@gmail.com>
2025-10-31 18:18:10 +0000

    Ejercicio 23 completo

---
+++ katas-js/ejercicio23.js
@@ -0,0 +1,29 @@
+/* Usa un bucle para crear 3 arrays de peliculas filtrados por categorias.
+Pelicula pequeña, menos de 100 minutos, pelicula mediana, mas de 100 minutos y menos de 200 y pelicula grande, mas de 200 minutos.
+Imprime cada array en por consola.*/
+const movies = [
+  { name: "Titan A.E.", durationInMinutes: 130 },
+  { name: "Nightmare before Christmas", durationInMinutes: 225 },
+  { name: "Inception", durationInMinutes: 165 },
+  { name: "The Lord of the Rings", durationInMinutes: 967 },
+  { name: "Star Wars: A New Hope", durationInMinutes: 214 },
+  { name: "Terminator", durationInMinutes: 140 },
+];
+
+const shortMovies = [];
+const mediumMovies = [];
+const longMovies = [];
+
+for (const movie of movies) {
+  if (movie.durationInMinutes < 100) {
+    shortMovies.push(movie);
+  } else if (movie.durationInMinutes >= 100 && movie.durationInMinutes < 200) {
+    mediumMovies.push(movie);
+  } else {
+    longMovies.push(movie);
+  }
+}
+
+console.log("Películas pequeñas (menos de 100 minutos):", shortMovies);
+console.log("Películas medianas (100-200 minutos):", mediumMovies);
+console.log("Películas grandes (más de 200 minutos):", longMovies);
